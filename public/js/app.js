310a90013a61e0a526c475995520873069579d19
root <root@localhost.localdomain>
2020-04-29 20:07:28 +0200

    Remove unnessessary console.log call

--- public/js/app.js
+++ public/js/app.js
@@ -1,6 +1,3 @@
-console.log('Client Side JavaScript is loaded');
-
-
 const weatherForm = document.querySelector('form');
 const search = document.querySelector('input');
 const messageOne = document.getElementById('message-1');
